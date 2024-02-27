2698366f6245e73253cb7a87504639af9e8c9374
Mohamed Aashir <tmohamedaashir@gmail.com>
2024-02-27 22:51:36 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './components/Navbar';
+
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <iframe src="https://drive.google.com/file/d/1OUHmSmrUPGUWrxdOlXJjXwOGcycMbbRT/preview" width="640" height="480"></iframe>
+      
+    </div>
+  )
+}
+
+export default App;
---
+++ src/lib/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA4tJDRu4WBzNKhzI69GHtpy638QUCXB2A",
+  authDomain: "video-test-99eed.firebaseapp.com",
+  projectId: "video-test-99eed",
+  storageBucket: "video-test-99eed.appspot.com",
+  messagingSenderId: "719370176333",
+  appId: "1:719370176333:web:b8605629f2b716bf869904"
+};
+
+export const app = initializeApp(firebaseConfig);
